9730fb5f64c1bba97a2179029238933b60b966ff
NguyenSon <son.nguyen@ntq-solution.com.vn>
2017-02-20 17:34:32 +0700

    demo threading

--- app/src/main/java/rx/learn/allen/allenlearnrx/MainActivity.java
+++ app/src/main/java/rx/learn/allen/allenlearnrx/MainActivity.java
@@ -3,158 +3,65 @@ package rx.learn.allen.allenlearnrx;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
-import java.util.concurrent.TimeUnit;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
+import rx.functions.Action1;
 import rx.functions.Func0;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 
 public class MainActivity extends AppCompatActivity {
     Integer[] items = {0, 1, 2, 3, 4};
     String[] strings = {"S", "M", "L"};
     Movie movie = new Movie("Hello Bro");
+    ImageView img;
+    TextView tv;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.img);
+        tv = (TextView) findViewById(R.id.tv);
 
-//        tryFrom();
-//        tryJust();
-//        tryDefer();
-//        tryInterval();
-        tryCreate();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getANumberObservable()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.io())
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String string) {
+                        Log.i("Operator thread", Thread.currentThread().getName());
+                        return string;
+                    }
+                })
+                .observeOn(Schedulers.newThread())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.i("Subscriber thread", Thread.currentThread().getName());
+                        tv.setText(s);
+                        Log.e("Subscriver thread", "not crashed");
+                    }
+                });
 
-    private void tryFrom() {
-        Observable.from(items).subscribe(new Subscriber<Integer>() {
-            @Override
-            public void onCompleted() {
-                log("OnCompleted");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                log("On Error");
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-                log("onNext: " + String.valueOf(integer));
-//                if (integer == 3) unsubscribe();
-            }
-        });
     }
 
-
-    private void tryJust() {
-        Observable.just("S", "M", "L").subscribe(new Subscriber<String>() {
+    private Observable<String> getANumberObservable() {
+        return Observable.defer(new Func0<Observable<String>>() {
             @Override
-            public void onCompleted() {
-                log("OnCompleted");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                log("On Error");
-            }
-
-            @Override
-            public void onNext(String string) {
-                log("onNext: " + string);
-            }
-        });
-//        Observable.just(strings).subscribe(new Subscriber<String[]>() {
-//            @Override
-//            public void onCompleted() {
-//
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//
-//            }
-//
-//            @Override
-//            public void onNext(String[] strings) {
-//                log("on Next");
-//            }
-//        });
-    }
-
-    public void tryDefer() {
-        Observable<Movie> observable = Observable.just(movie);
-        Observable<Movie> observable1 = Observable.defer(new Func0<Observable<Movie>>() {
-            @Override
-            public Observable<Movie> call() {
-                return Observable.just(movie);
-            }
-        });
-        movie = new Movie("Hello SML");
-        observable.subscribe(new Subscriber<Movie>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(Movie movie1) {
-                log(movie1.getTitle());
-            }
-        });
-    }
-
-    public void tryInterval() {
-        Observable.interval(2, TimeUnit.SECONDS).subscribe(new Subscriber<Long>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(Long aLong) {
-                log(String.valueOf(aLong));
-                if (aLong == 10) unsubscribe();
-            }
-        });
-    }
-
-    private void tryCreate() {
-        Observable.create(new Observable.OnSubscribe<String>() {
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("S");
-                subscriber.onNext("M");
-                subscriber.onNext("L");
-                subscriber.onCompleted();
-            }
-        }).subscribe(new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-                log("On Complete");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-                log("On next: "+ s);
+            public Observable<String> call() {
+                Log.i("Observable thread", Thread.currentThread().getName());
+                return Observable.just("Hello");
             }
         });
     }
